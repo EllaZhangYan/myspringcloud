87f36d004a75d3bf8ddf6b95731dda79fa3f7f1f
EllaZhangYan <13554843872@163.com>
2020-01-07 08:44:27 +0800

    Revert "修改了MainController"
    This reverts commit 0fb734b0

--- Itaobao-manage/src/main/java/com/hzit/controller/MainController.java
+++ Itaobao-manage/src/main/java/com/hzit/controller/MainController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class MainController {
-    //这是访问后台管理界面的主页面
 	@RequestMapping("query")
    public String query() {
 	   return "main";
